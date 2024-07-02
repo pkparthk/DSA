d5b313e2833309b618761517791e1c7b77f35046
pkparthk <parthkothari75@gmail.com>
2024-07-03 00:40:43 +0530

    update

--- 01pattern.cpp
+++ 01pattern.cpp
@@ -111,6 +111,38 @@ void pattern8(int n){
         cout << endl;
     }
 }
+
+void pattern10(int n){
+    for (int i = 0; i <= 2 * n - 1;i++){
+        int stars = i;
+        if(i > n){
+            stars = 2 * n - i;
+        }
+        for (int j = 1; j <= stars; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;   
+    }
+}
+void pattern11(int n){
+    int start = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (i%2==0){
+            start = 1;
+        }
+        else{
+            start = 0;
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            cout << start;
+            start = 1 - start;
+        }
+        cout << endl;
+    }       
+}
 int main()
 {
     // int n;
@@ -192,14 +224,44 @@ int main()
     // }
 
     // Pattern 8
+    // for (int i = 0; i < t; i++)
+    // {
+    //     int n;
+    //     // cout << "Enter te value for pattern";
+    //     cin >> n;
+    //     pattern8(n);
+    //     cout << endl;
+    // }
+
+    // Patern 9
+    // for (int i = 0; i < t; i++)
+    // {
+    //     int n;
+    //     // cout << "Enter te value for pattern";
+    //     cin >> n;
+    //     pattern7(n);
+    //     pattern8(n);
+    //     cout << endl;
+    // }
+
+    // Pattern 10
+    // for (int i = 0; i < t; i++)
+    // {
+    //     int n;
+    //     // cout << "Enter te value for pattern";
+    //     cin >> n;
+    //     pattern10(n);
+    //     cout << endl;
+    // }
+
+    // Pattern 11
     for (int i = 0; i < t; i++)
     {
         int n;
         // cout << "Enter te value for pattern";
         cin >> n;
-        pattern8(n);
+        pattern11(n);
         cout << endl;
     }
-
     return 0;
 }
